1dfcd99bb3d5aec15c33533bd7b61bbd06274008
Vitor <ciottovitorl@gmail.com>
2026-04-29 20:18:57 -0300

    Aula dia 29/04

---
+++ topico071/topico071.php
@@ -0,0 +1,25 @@
+<?php
+echo "<h1>While</h1>";
+$i=11;
+while($i<15){
+    echo " $i";
+    $i++;
+}
+/*
+Saída : 0 1 2 3 4 
+1) valor inicial 0 
+2)condição? $i<=5
+3)contador? $i++
+4)quantas vezes o lopping foi executado? 6
+5)valor que torna a condição como falsa? 6
+----------------------------------------------
+Saída : 11 12 13 14
+
+1) valor inicial 0 
+2)condição? $i<=5
+3)contador? $i++
+4)quantas vezes o lopping foi executado? 6
+5)valor que torna a condição como falsa? 6
+*/
+
+?>
